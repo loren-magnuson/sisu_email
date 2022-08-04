5dd5b47e69f38f11e43aca5b60a30b836727f507
Loren Magnuson <lorenjmagnuson@gmail.com>
2022-08-03 22:48:06 -0400

    blah

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sisu_email",                     # This is the name of the package
-    version="0.0.6",                        # The initial release version
+    version="0.0.6a",                        # The initial release version
     author="Loren Magnuson",                     # Full name of the author
-    description="Quicksample Test Package for SQLShack Demo",
+    description="sisu_email - some abstractions around the standard python email library",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    packages=['sisu_email'],    # List of all python modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
